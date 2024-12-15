9f9a1afa046f29ea497146598b9abfd97e692efd
Josh <jnhartshorne@gmail.com>
2024-12-15 16:19:25 +0000

    Add basic login form component

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
   import { Head } from '@inertiajs/vue3'
 
-  import { Button } from '@/components/ui/button'
+import Login from '@/components/ui-macro/login/Login.vue'
 </script>
 
 <template>
   <Head title="Homepage" />
+  
+  <main class="min-h-screen flex items-center justify-center">
+    <Login />
+  </main>
 </template>
